0c16841058d5ac07f88b5211da8b315ed7d3c4d5
dietcokechan <dietcokechan@gmail.com>
2024-05-20 22:44:49 +0300

    basic setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$("p").add("Holberton Dashboard").appendTo("body");
+$("p").add("Dashboard data for the students").appendTo("body");
+$("p").add("Copyright - Holberton School").appendTo("body");
